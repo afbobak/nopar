924a14d2147b194a1e99da97940eb9aa291db944
Andreas F. Bobak <bobak@abstrakt.ch>
2014-04-20 09:51:06 +0800

    Fix test helpers for finding routes in express4

--- test/helpers.js
+++ test/helpers.js
@@ -3,13 +3,26 @@
 /*! Copyright (C) 2013 by Andreas F. Bobak, Switzerland. All Rights Reserved. !*/
 "use strict";
 
-exports.findCall = function(calls, path) {
-  var i;
-  for (i = calls.length - 1; i >= 0; i--) {
-    if (calls[i].path === path) {
-      return calls[i];
+exports.findRoute = function(server, path) {
+  /*jslint nomen: true */
+  var stack = server._router.stack;
+
+  var i, layer;
+  for (i = stack.length - 1; i >= 0; i--) {
+    layer = stack[i];
+    if (layer.route && layer.route.path === path) {
+      return layer.route;
     }
   }
   return;
 };
 
+exports.findHandle = function(route, method) {
+  var i, stack = route.stack;
+  for (i = stack.length - 1; i >= 0; i--) {
+    if (stack[i].method === method) {
+      return stack[i].handle;
+    }
+  }
+  return;
+};
