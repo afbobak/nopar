3c991ddf49504accda3b3b6d75a4aea472e9571f
Andreas F. Bobak <bobak@abstrakt.ch>
2015-02-09 14:14:58 +0800

    fix: add daemon to package.json and create nopar.pid

--- nopar.js
+++ nopar.js
@@ -4,4 +4,9 @@ if (process.argv.length === 3 && process.argv[2] === '--detach') {
   require('daemon')();
 }
 
+var NOPAR_RUN_PATH = process.env['NOPAR_RUN_PATH'] || '.';
+var fs = require("fs");
+var path = require("path");
+fs.writeFileSync(path.join(NOPAR_RUN_PATH, "nopar.pid"), process.pid);
+
 require('./lib/server').start();
